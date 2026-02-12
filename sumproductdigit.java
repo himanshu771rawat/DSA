ff231ed8138eac2f3434936a26d4604c1766394b
himanshu771rawat <himanshu771ra@gmail.com>
2026-02-12 12:45:50 +0530

    Add files via upload

---
+++ sumproductdigit.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class sumproductdigit {
+
+    public static int subtractProductAndSum(int n) {
+        int product = 1;
+        int sum = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+            product *= digit;
+            sum += digit;
+            n /= 10;
+        }
+
+        return product - sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        int result = subtractProductAndSum(n);
+
+        System.out.println("Result: " + result);
+    }
+}
